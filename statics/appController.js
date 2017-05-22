0c9c8a0b29ddf96a2fe70f0436fbb9fb8a0fc08f
Natarajan <Arulmozhimanikandan.Natarajan@rabobank.nl>
2017-05-22 19:32:58 +0200

    timer included

--- statics/appController.js
+++ statics/appController.js
@@ -1,22 +1,42 @@
 angular.module('app').controller('appController', function ($scope, mySocket) {
 
-    $scope.users = [];
-    $scope.quiz = {};
-    $scope.rightAnswerUsers = [];
+    function init() {
+        $scope.users = [];
+        $scope.quiz = {};
+        $scope.rightAnswerUsers = [];
+        $scope.showQuizDashboard = false;
 
-    if (localStorage.getItem('user')) {
-        $scope.getNickName = false;
-        $scope.nickName = localStorage.getItem('user');
-    } else {
-        $scope.getNickName = true;
+        if (localStorage.getItem('user')) {
+            $scope.getNickName = false;
+            $scope.nickName = localStorage.getItem('user');
+        } else {
+            $scope.getNickName = true;
+        }
+        if ($scope.nickName == 'AdminCreatorQuiz' && !$scope.getNickName) {
+            $scope.showCreator = true;
+            $scope.admin = true;
+        } else {
+            if (!$scope.getNickName) {
+                $scope.participant = true;
+            }
+        }
     }
-    if ($scope.nickName == 'AdminCreatorQuiz') {
-        $scope.showCreator = true;
-        $scope.admin = true;
-    } else {
-        $scope.participant = true;
+    $scope.startTimer = function () {
+        var d1 = new Date(),
+            d2 = new Date(d1);
+        d2.setSeconds(d1.getSeconds() + 32);
+        var countDownDate = d2.getTime();
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+           $scope.reminingSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+           $scope.$apply();
+            if (distance < 0) {
+                clearInterval(x);
+              
+            }
+        }, 1000);
     }
-
     mySocket.on('online', function (msg) {
         $scope.users.push(msg.username);
     });
@@ -26,6 +46,7 @@ angular.module('app').controller('appController', function ($scope, mySocket) {
         mySocket.emit('online', { username: $scope.nickName });
         $scope.hideModel = true;
         localStorage.setItem('user', $scope.nickName);
+        init();
     }
 
     $scope.sendAnswer = function (s, k, a) {
@@ -51,6 +72,7 @@ angular.module('app').controller('appController', function ($scope, mySocket) {
     }
 
     mySocket.on('quizReady', function (msg) {
+        $scope.startTimer();
         $scope.showQuiz = true;
         $scope.quizObject = msg.quiz;
     })
@@ -65,6 +87,7 @@ angular.module('app').controller('appController', function ($scope, mySocket) {
         $scope.answered = false;
         $scope.quiz = {};
         $scope.rightAnswerUsers = [];
-    })
+    });
 
+    init();
 })
